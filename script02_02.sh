e0feebd1fb7e66a2d283fdb30a649e547a653f92
ndtiralongo <Andreatiralongo12@gmail.com>
2015-03-09 15:18:48 +0100

    Update script02_02.sh

--- script02_02.sh
+++ script02_02.sh
@@ -2,5 +2,13 @@
 
 #
 # Questo script riceve un argomento. Esso contiene un _prefisso_ per il nome di una directory.
+echo "Inserire argomento"
+exit 1
 # Lo script crea 10 directory, nella directory corrente, con il prefisso fornito e un numero.
 # Esempio: prefisso = toscano -> directory: toscano1, toscano2, toscano3, ... toscano10
+for j in $(seq 10)
+do
+	mkdir $@$j
+done
+
+exit 0
